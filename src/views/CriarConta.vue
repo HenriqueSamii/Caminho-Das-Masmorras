c3f8aea26105d7362c17c94720764d12f4f4c34b
hen samii <henriquesamii@hotmail.com>
2020-03-15 13:54:10 -0300

    base de login_eventos_logout_critar conta

---
+++ src/views/CriarConta.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="criarConta">
+    
+  </div>
+</template>
+
+<script>
+
+//import Header from "./components/Header.vue";
+export default {
+  name: "CriarConta",
+  data: function(){
+    return{};
+  }/* ,
+  components: {
+    Header
+  } */
+};
+</script>
+<style>
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="login">
+    
+  </div>
+</template>
+
+<script>
+
+//import Header from "./components/Header.vue";
+export default {
+  name: "Login",
+  data: function(){
+    return{};
+  }/* ,
+  components: {
+    Header
+  } */
+};
+</script>
+<style>
+</style>
