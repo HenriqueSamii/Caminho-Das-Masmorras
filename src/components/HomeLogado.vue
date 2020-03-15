e3d77b5ac43e709accd7b18a4349d86df681a9b3
hen samii <henriquesamii@hotmail.com>
2020-03-15 14:12:31 -0300

    base de login

---
+++ src/components/HomeLogado.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="homeLogado">
+    
+  </div>
+</template>
+
+<script>
+
+//import Header from "./components/Header.vue";
+export default {
+  name: "HomeLogado",
+  data: function(){
+    return{};
+  }/* ,
+  components: {
+    Header
+  } */
+};
+</script>
+<style>
+</style>
