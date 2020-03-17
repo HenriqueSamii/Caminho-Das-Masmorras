88d33f699ed1778a41c77e24a4aecbd9fb177e6b
hen samii <henriquesamii@hotmail.com>
2020-03-17 13:32:09 -0300

    vuex e vue mostrar eventos teste

--- src/store/index.js
+++ src/store/index.js
@@ -16,8 +16,8 @@ export default new Vuex.Store({
       {id:3, criadorId:"2",titulo:"post 2",corpo:"E ai, quem topa uma mesa de D&D amanhã?", dataDeCriacao:"2020-03-20T18:25:43.511Z"}
     ],
     eventos:[
-      {id:1, criadorId:"1",local:"17, R. Nelson Mandela, 5 - Botafogo, Rio de Janeiro - RJ, 22713-588",custo:"5 R$ por cabeça",inicioDoEvento:"2020-03-12T15:00:00.511Z",fimDoEvento:"2020-03-12T18:30:00.511Z", NumeroMaximoDePessoas:4,idDeParticipantes:[1,2],sobreOEcento:"Evento de 40K com tres mesas prontas para jogar"},
-      {id:2, criadorId:"2",local:"Rua Benjamin Constant, 48 - Glória, Rio de Janeiro - RJ, 20241-150",custo:"Gratuito",inicioDoEvento:"2020-03-21T15:00:00.511Z",fimDoEvento:"2020-03-21T1:30:00.511Z", NumeroMaximoDePessoas:6,idDeParticipantes:[1,2],sobreOEcento:"E ai, quem topam uma D&D amanhã?"}
+      {id:1, criadorId:"1",local:"17, R. Nelson Mandela, 5 - Botafogo, Rio de Janeiro - RJ, 22713-588",custo:"5 R$ por cabeça",inicioDoEvento:"2020-03-12T15:00:00.511Z",fimDoEvento:"2020-03-12T18:30:00.511Z", NumeroMaximoDePessoas:4,idDeParticipantes:[1],sobreOEvento:"Evento de 40K com tres mesas prontas para jogar"},
+      {id:2, criadorId:"2",local:"Rua Benjamin Constant, 48 - Glória, Rio de Janeiro - RJ, 20241-150",custo:"Gratuito",inicioDoEvento:"2020-03-21T15:00:00.511Z",fimDoEvento:"2020-03-21T1:30:00.511Z", NumeroMaximoDePessoas:6,idDeParticipantes:[],sobreOEvento:"E ai, quem topam uma D&D amanhã?"}
     ]
   },
   getters:{
--- src/views/Eventos.vue
+++ src/views/Eventos.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="eventos">
-    
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">local</th>
+          <th scope="col">custo</th>
+          <th scope="col">sobreOEvento</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="evento in todosEventos" :key="evento">
+          <!-- <th scope="row">1</th> -->
+          <td>{{evento.id}}</td>
+          <td>{{evento.local}}</td>
+          <td>{{evento.custo}}</td>
+          <td>{{evento.sobreOEvento}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-
 //import Header from "./components/Header.vue";
 export default {
   name: "Eventos",
-  data: function(){
-    return{};
-  }/* ,
+  data: function() {
+    return {};
+  },
+  computed: {
+    todosEventos() {
+      return this.$store.getters.allEventos;
+    }
+  }
+  /* ,
   components: {
     Header
   } */
